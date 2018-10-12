2c964d1840abf6406f2f5aef794535eeafa1c0d9
Hungnguyen15595 <37910067+Hungnguyen15595@users.noreply.github.com>
2018-10-11 21:35:43 -0700

    Create Project in the local repository

---
+++ smartAttedanceiOSApp/smartAttedanceiOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smartAttedanceiOSApp
+//
+//  Created by Hung Nguyen on 10/11/18.
+//  Copyright © 2018 Hung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
