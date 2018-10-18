76667feba0cb9e327b1a62c71a08998504bb27b6
Hungnguyen15595 <37910067+Hungnguyen15595@users.noreply.github.com>
2018-10-17 19:14:58 -0700

    Added Manage Classes, Take Attendance, Attendance Record ViewController

---
+++ SmartAttendance/SmartAttendance/ManageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ManageViewController.swift
+//  SmartAttendance
+//
+//  Created by Hung Nguyen on 10/12/18.
+//  Copyright © 2018 Hung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ManageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
