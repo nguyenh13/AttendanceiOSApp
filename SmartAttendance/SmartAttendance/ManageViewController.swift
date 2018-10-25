ff00dc9676677e9ea5fc35b55b23acff995c9ba6
Hungnguyen15595 <37910067+Hungnguyen15595@users.noreply.github.com>
2018-10-25 01:01:21 -0700

    Implement saving data using CoreData. Fixed bug CoreData does not update after terminate the app.

--- SmartAttendance/SmartAttendance/ManageViewController.swift
+++ SmartAttendance/SmartAttendance/ManageViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ManageViewController.swift
+//  ManageClasses.swift
 //  SmartAttendance
 //
 //  Created by Hung Nguyen on 10/12/18.
@@ -7,24 +7,101 @@
 //
 
 import UIKit
+import CoreData
 
-class ManageViewController: UIViewController {
-
+class ManageViewController : UIViewController, UITableViewDataSource{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var classList = [Class]()
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if classList.count == 0 {
+        createAlert(title: "Add class", message: "Click plus button to add class")
+        }
+        tableView.dataSource = self
+        
+        let fetchRequest: NSFetchRequest<Class> = Class.fetchRequest()
+        
+        do {
+            let classList = try PersistenceService.context.fetch(fetchRequest)
+            self.classList = classList
+            self.tableView.reloadData()
+        } catch {}
+       
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onAddTapped(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add Class", message: nil, preferredStyle: .alert)
+        alert.addTextField { (classListTF) in
+            classListTF.placeholder = "Enter Class"
+        }
+        let action = UIAlertAction(title: "Add", style: .default) { (_) in
+            guard let list = alert.textFields?.first?.text else { return }
+            print(list)
+//            self.add(list)
+            let subject = Class(context: PersistenceService.context)
+            subject.class_name = list
+            PersistenceService.saveContext()
+            self.classList.append(subject)
+            self.tableView.reloadData()
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
     }
-    */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return classList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "classCell", for: indexPath)
+        
+        cell.textLabel?.text = classList[indexPath.row].class_name
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else {return}
+        classList.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .fade)
+        let subject = classList[indexPath.row]
+        PersistenceService.context.delete(subject)
+        PersistenceService.saveContext()
+        
+        let fetchRequest: NSFetchRequest<Class> = Class.fetchRequest()
+        do {
+            let classList = try PersistenceService.context.fetch(fetchRequest)
+            self.classList = classList
+        } catch {}
+        self.tableView.reloadData()
+        print("Delete \(subject)")
+    }
 
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+    }
+    
+    //Alert function to add class
+    func createAlert (title:String, message:String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {(action) in alert.dismiss(animated: true, completion: nil)}))
+        
+        self.present(alert, animated: true,completion: nil)
+    }
+    
 }
+
